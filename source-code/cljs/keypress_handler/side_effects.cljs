5046055e7e3110da394f0d3f98cbd22612d30065
Bit Handshake <adam@monotech.hu>
2023-12-16 15:43:40 +0100

    1.9.1.0

--- source-code/cljs/keypress_handler/side_effects.cljs
+++ source-code/cljs/keypress_handler/side_effects.cljs
@@ -44,12 +44,12 @@
   ; @ignore
   ;
   ; @description
-  ; XXX#1160
   ; Stores the event properties (or overwrites it when a keypress event registered again).
   ;
   ; @param (keyword) event-id
   ; @param (map) event-props
   [event-id event-props]
+  ; @NOTE (#1160)
   (swap! state/KEYPRESS-EVENTS assoc event-id event-props))
 
 (defn remove-event-props!
@@ -72,16 +72,15 @@
   ; Every registered key-code has a vector in the cache with the event IDs of
   ; the events which use that key-code.
   ;
-  ; XXX#1160
-  ; If a keypress event registered again, the cache doesn't store its ID again
-  ; (to avoid duplicates in the cache).
-  ;
   ; @param (keyword) event-id
   ; @param (map) event-props
   ; {:key-code (integer)
   ;  :on-keydown (function)(opt)
   ;  :on-keyup (function)(opt)}
   [event-id {:keys [key-code on-keydown on-keyup]}]
+  ; @NOTE (#1160)
+  ; If a keypress event registered again, the cache doesn't store its ID again
+  ; (to avoid duplicates in the cache).
   (if on-keydown (swap! state/EVENT-CACHE update-in [key-code :keydown-events] vector/conj-item-once event-id))
   (if on-keyup   (swap! state/EVENT-CACHE update-in [key-code :keyup-events]   vector/conj-item-once event-id)))
 
@@ -230,7 +229,7 @@
   ;
   ; @param (integer) key-code
   [key-code]
-  ; BUG#5050
+  ; @BUG (#5050)
   ; https://stackoverflow.com/questions/25438608/javascript-keyup-isnt-called-when-command-and-another-is-pressed
   (unmark-key-as-pressed! key-code)
   (doseq [on-keyup (env/get-keyup-events key-code)]
